19bd4fa546bb343e98854d8620532895c0e1fda1
ms-acar <msacar23@gmail.com>
2025-06-24 11:40:46 +0300

    gif konuldu & düzenlemeler

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -8,3 +8,15 @@ window.addEventListener('scroll', () => {
     navbar.classList.add('bg-transparent');
   }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('[data-collapse-toggle="mobile-menu-2"]');
+    const mobileMenu = document.getElementById('mobile-menu-2');
+
+    if (menuButton && mobileMenu) {
+        menuButton.addEventListener('click', function (event) {
+            event.stopPropagation();
+            mobileMenu.classList.toggle('hidden');
+        });
+    }
+});
